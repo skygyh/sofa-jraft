80979f3fb883b74636c48bc3478c4384e943cd1b
gongy2 <yunhai.gong@emc.com>
2020-09-10 08:24:59 +0000

    update script for pmem benchmark

--- jraft-example/bin/server_benchmark_start.sh
+++ jraft-example/bin/server_benchmark_start.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+sudo mount -o dax /dev/pmem1 /mnt/mem/
+sudo rm -rf /mnt/mem/*
+sudo systemctl stop SuSEfirewall2.service
+sudo systemctl disable SuSEfirewall2.service
+sudo /sbin/SuSEfirewall2 off
+echo "$1"
 
 BASE_DIR=$(dirname $0)/..
 CLASSPATH=$(echo $BASE_DIR/lib/*.jar | tr ' ' ':')
@@ -27,7 +33,7 @@ if [[ $JAVA_VERSION =~ "1.8" ]]; then
     JAVA_OPT_1="-server -Xms6g -Xmx6g -Xmn3g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m -Xss256k -XX:MaxDirectMemorySize=16g "
   fi
 
-  JAVA_OPT_2="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:+DisableExplicitGC"
+  JAVA_OPT_2="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=benchmark.hprof -XX:+UnlockDiagnosticVMOptions -XX:+ExitOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -Xloggc:benchmark.gc.log"
 else
   echo "Error, not support java version : $JAVA_VERSION"
   exit 1
@@ -40,9 +46,8 @@ cat <<EOF | xargs echo > $JAVA_CONFIG
 ${JAVA_OPTS}
 -cp $CLASSPATH
 com.alipay.sofa.jraft.benchmark.BenchmarkBootstrap
-server
-$1
-$2
+server 10.255.93.15:18091,10.255.93.16:18091,10.255.93.17:18091 ../../../config/benchmark_server_pmem.yaml
+true $1 10 0 1000000 64 1024
 EOF
 
 JAVA_CONFIG=$(cat $JAVA_CONFIG | xargs echo)
@@ -53,4 +58,4 @@ JAVA="$JAVA_HOME/bin/java"
 
 HOSTNAME=`hostname`
 
-nohup $JAVA $JAVA_CONFIG >> server_stdout 2>&1 &
+nohup $JAVA $JAVA_CONFIG > server_stdout 2>&1 &
