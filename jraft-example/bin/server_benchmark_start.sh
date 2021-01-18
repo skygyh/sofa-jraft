3952efd818aee12adf37f1b7e42a5115accdc3c7
yunhai <yunhai.gong@dell.com>
2021-01-18 02:51:05 +0000

    update script

--- jraft-example/bin/server_benchmark_start.sh
+++ jraft-example/bin/server_benchmark_start.sh
@@ -32,12 +32,15 @@ if [[ $JAVA_VERSION =~ "1.8" ]]; then
     JAVA_OPT_1="-server -Xms6g -Xmx6g -Xmn3g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m -Xss256k -XX:MaxDirectMemorySize=16g "
   fi
 
-  JAVA_OPT_2="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:+DisableExplicitGC"
+#  JAVA_OPT_2="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:+DisableExplicitGC"
+  JAVA_OPT_2="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./log/blobsvc-$$.hprof -XX:+UnlockDiagnosticVMOptions -XX:GuaranteedSafepointInterval=30000 -Djdk.nio.maxCachedBufferSize=1048576  -XX:-UseBiasedLocking -Dsun.net.inetaddr.ttl=0 -XX:MaxTenuringThreshold=8 -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:+ExplicitGCInvokesConcurrent -XX:+CMSScavengeBeforeRemark -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ExitOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -Xloggc:./log/gc-$((($(ls ./log/gc-*.log.0* -t 2>/dev/null | head -n1 | sed 's/.*-gc-\([0-9]\).*/\1/')+1)%10)).log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=50M"
+
 else
   echo "Error, not support java version : $JAVA_VERSION"
   exit 1
 fi
 
+#example: ./server_benchmark_start.sh  10.255.93.15:18091,10.255.93.16:18091,10.255.93.17:18091 ../config/benchmark_server_pmem.yaml false
 JAVA_OPTS="${JAVA_OPT_1} ${JAVA_OPT_2}"
 
 JAVA_CONFIG=$(mktemp XXXXXXXX)
@@ -60,3 +63,5 @@ JAVA="$JAVA_HOME/bin/java"
 HOSTNAME=`hostname`
 
 nohup $JAVA $JAVA_CONFIG >> server_stdout 2>&1 &
+
+tailf server_stdout
